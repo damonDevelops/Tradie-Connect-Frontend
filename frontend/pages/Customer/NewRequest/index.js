f3bc8a4b1cba0033874c76f4935e66a3b29a05de
essej93 <86395968+essej93@users.noreply.github.com>
2023-04-21 12:41:07 +1000

    Updated link package to next/link

--- frontend/pages/Customer/NewRequest/index.js
+++ frontend/pages/Customer/NewRequest/index.js
@@ -1,5 +1,6 @@
 import NewRequest from "/components/NewRequest/NewRequestForm";
 import SideBar from "../../../components/SideBarTemplate";
+
 export default function newrequest() {
   return (
     <div>
--- frontend/pages/Customer/index.js
+++ frontend/pages/Customer/index.js
@@ -14,7 +14,8 @@ import Badge from "@mui/material/Badge";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import Link from "@mui/material/Link";
+//import Link from "@mui/material/Link";
+import Link from "next/link";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import NotificationsIcon from "@mui/icons-material/Notifications";
@@ -182,15 +183,14 @@ function CustomerDash() {
               </ListItemButton>
               <Collapse in={newOpen} timeout="auto" unmountOnExit>
                 <List component="div" disablePadding>
-                  <ListItemButton
-                    sx={{ pl: 4 }}
-                    href="./NewRequest/NewRequestIndex"
-                  >
-                    <ListItemIcon>
-                      <StarBorder />
-                    </ListItemIcon>
-                    <ListItemText primary="New Request" />
-                  </ListItemButton>
+                  <Link href="./Customer/NewRequest" passHref legacyBehavior>
+                    <ListItemButton sx={{ pl: 4 }}>
+                      <ListItemIcon>
+                        <StarBorder />
+                      </ListItemIcon>
+                      <ListItemText primary="New Request" />
+                    </ListItemButton>
+                  </Link>
                 </List>
               </Collapse>
               <Collapse in={newOpen} timeout="auto" unmountOnExit>
