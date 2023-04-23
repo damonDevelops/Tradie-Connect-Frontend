e9f20fbc3e81454ecc910b70a6a516eb2a3bf57c
essej93 <86395968+essej93@users.noreply.github.com>
2023-04-23 18:54:46 +1000

    Added current requests table Table updates dynamically depending on how many requests there are Updated to allow navigation of service requests if there are more than 5

--- frontend/components/Requests/CurrentRequest.js
+++ frontend/components/Requests/CurrentRequest.js
@@ -1,23 +1,29 @@
 import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
 
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import axios from "axios";
 
 import { Link, TextField } from "@mui/material";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogTitle from "@mui/material/DialogTitle";
 import { useState } from "react";
 import { useEffect } from "react";
-import { Stack } from "@mui/material";
-import { Snackbar } from "@mui/material";
-import { Alert } from "@mui/material";
+
+// for displaying data:
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TablePagination from "@mui/material/TablePagination";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+
 //import { DataGrid } from "@mui/x-data-grid";
 
 const theme = createTheme();
@@ -96,46 +102,95 @@ export default function CurrentRequest() {
   //     )
   // );
 
+  // maps the data from the request into a rows array with only the data required to be shown
+  const rows = data.map(
+    ({ id, serviceType, status, requestedDate, scheduledEndDate, cost }) => ({
+      id,
+      serviceType,
+      status,
+      requestedDate,
+      scheduledEndDate,
+      cost,
+    })
+  );
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Paper
-        sx={{
-          p: 2,
-          display: "flex",
-          flexDirection: "column",
-          height: 750,
-        }}
-      >
-        <Typography variant="h4" gutterBottom>
-          Current Requests
-        </Typography>
-        <p>Request Type: {requestType}</p>
-        <p>Request Status: {requestStatus}</p>
-        <p>Request Start Date: {requestStartDate}</p>
-        <p>Request End Date: {requestEndDate}</p>
-        <p>Request Cost: {requestCost}</p>
-        <p>Request Description: {requestDescription}</p>
-      </Paper>
-
-      <Paper
-        sx={{
-          p: 2,
-          display: "flex",
-          flexDirection: "column",
-          height: 750,
-        }}
-      >
-        <Typography variant="h4" gutterBottom>
-          Current Requests lists
-        </Typography>
-        {data.map((serviceRequests) => (
-          <Typography>
-            <Link>id:{serviceRequests.id}</Link>
-            Service Type: {serviceRequests.serviceType}
-          </Typography>
-        ))}
-      </Paper>
+      <Typography variant="h4" gutterBottom>
+        Current Requests
+      </Typography>
+      <RequestTable data={rows} />
     </ThemeProvider>
+  );
+}
+
+function RequestTable({ data }) {
+  // styles for the header row
+  const headerStyles = {
+    textAlign: "center",
+    fontWeight: "bold",
+  };
+
+  // styles for cell content
+  const cellStyles = {
+    textAlign: "center",
+  };
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const rowsToDisplay = data.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
+
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minwidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell sx={headerStyles}>Service Request (ID)</TableCell>
+            <TableCell sx={headerStyles}>Work Type</TableCell>
+            <TableCell sx={headerStyles}>Work Status</TableCell>
+            <TableCell sx={headerStyles}>Start Date</TableCell>
+            <TableCell sx={headerStyles}>Finish Date</TableCell>
+            <TableCell sx={headerStyles}>Cost ($)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rowsToDisplay.map((row, index) => (
+            <TableRow key={index}>
+              <TableCell sx={{ width: "20%", textAlign: "center" }}>
+                {row.id}
+              </TableCell>
+              <TableCell sx={cellStyles}>{row.serviceType}</TableCell>
+              <TableCell sx={cellStyles}>{row.status}</TableCell>
+              <TableCell sx={cellStyles}>{row.requestedDate}</TableCell>
+              <TableCell sx={cellStyles}>{row.scheduledEndDate}</TableCell>
+              <TableCell sx={cellStyles}>{row.cost}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </TableContainer>
   );
 }
