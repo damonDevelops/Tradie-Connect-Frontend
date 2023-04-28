aee945fb7766b965f26a8ffa3aa0fa30e272f05f
essej93 <86395968+essej93@users.noreply.github.com>
2023-04-28 18:15:28 +1000

    added fetchData hook updated ViewCurrentRequest - Request was working with response data however, is currently not working as axios request is failing. Need to double check the request

--- frontend/components/Requests/ViewCurrentRequest.js
+++ frontend/components/Requests/ViewCurrentRequest.js
@@ -5,12 +5,12 @@ import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import axios from "axios";
-
-import { Link, TextField } from "@mui/material";
-import { useState } from "react";
 import { useEffect } from "react";
 
 import { useRouter } from "next/router";
+
+import { Link, TextField } from "@mui/material";
+import { useState } from "react";
 
 // for displaying data:
 import Box from "@mui/material/Box";
@@ -26,6 +26,8 @@ import TablePagination from "@mui/material/TablePagination";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
+import useFetchData from "../hooks/fetchData";
+
 //import { DataGrid } from "@mui/x-data-grid";
 
 const theme = createTheme();
@@ -34,6 +36,11 @@ export default function ViewRequest() {
   const router = useRouter();
   const requestId = router.query.id;
   const fromRequests = router.query.fromRequests;
+  const fetchURL =
+    "http://localhost:8080/api/service-requests/" + router.query.id;
+
+  const { data: responseData } = useFetchData(fetchURL);
+  const [displayData, setDisplayData] = editDisplayData(responseData);
 
   useEffect(() => {
     if (!fromRequests) {
@@ -43,6 +50,8 @@ export default function ViewRequest() {
 
   console.log(requestId);
   console.log(fromRequests);
+  console.log(responseData);
+  console.log(displayData);
 
   return (
     <ThemeProvider theme={theme}>
@@ -57,6 +66,60 @@ export default function ViewRequest() {
       >
         <Typography variant="h4" gutterBottom>
           View Request: {requestId}
+          {/* <Box sx={{ p: 2 }}>
+            {displayData ? (
+              <>
+                <Typography variant="h4" sx={{ mb: 2 }}>
+                  Service Request Details
+                </Typography>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  <TextField
+                    label="Request ID"
+                    value={displayData.id}
+                    disabled
+                    sx={{ mb: 2 }}
+                  />
+                  <TextField
+                    label="Date Submitted"
+                    value={displayData.requestedDate}
+                    disabled
+                    sx={{ mb: 2 }}
+                  />
+                  <TextField
+                    label="Scheduled Date"
+                    value={displayData.scheduledStartDate}
+                    disabled
+                    sx={{ mb: 2 }}
+                  />
+                  <TextField
+                    label="Scheduled Finish"
+                    value={displayData.scheduledStartDate}
+                    disabled
+                    sx={{ mb: 2 }}
+                  />
+                  <TextField
+                    label="Status: "
+                    value={displayData.status}
+                    disabled
+                    sx={{ mb: 2 }}
+                  />
+                  <TextField
+                    label="Description"
+                    value={displayData.description}
+                    disabled
+                    sx={{ mb: 2 }}
+                  />
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Button onClick={handleBackButtonClick}>Back</Button>
+                  </Box>
+                </Box>
+              </>
+            ) : (
+              <Typography variant="h6">Loading...</Typography>
+            )}
+          </Box> */}
           {/* {fromRequests ? (
             <Typography>
               you came here from the current requests page
@@ -70,4 +133,47 @@ export default function ViewRequest() {
       </Paper>
     </ThemeProvider>
   );
+}
+
+export function editDisplayData(data) {
+  const [displayData, setDisplayData] = useState({
+    serviceType: "",
+    requestedDate: "",
+    requestedTime: "",
+    scheduledStartDate: "",
+    scheduledStartTime: "",
+    scheduledEndDate: "",
+    scheduledEndTime: "",
+    cost: "",
+    description: "",
+  });
+
+  useEffect(() => {
+    const capitaliseWords = (str) => {
+      return str
+        .toLowerCase()
+        .split("_")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+    };
+
+    const formatDate = (date) => {
+      return date[2] + "/" + date[1] + "/" + date[0];
+    };
+
+    setDisplayData({
+      serviceType: capitaliseWords(data.serviceType),
+      requestedDate: formatDate(data.requestedDate),
+      requestedTime: data.requestedTime,
+      scheduledStartDate: formatDate(data.scheduledStartDate),
+      scheduledStartTime: data.scheduledStartTime,
+      scheduledEndDate: formatDate(data.scheduledEndDate),
+      scheduledEndTime: data.scheduledEndTime,
+      cost: data.cost,
+      description: data.description,
+      status: capitaliseWords(data.status),
+    });
+  }, [data]);
+
+  return [displayData, setDisplayData];
 }
---
+++ frontend/components/hooks/fetchData.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetchData = (url) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url, {
+          responseType: "json",
+          withCredentials: true,
+        });
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  // console.log(data);
+
+  return { data };
+};
+
+export default useFetchData;
+
+// to use this in another component:
+// import useFetchData from "./useFetchData";
+
+// const MyPage = () => {
+//   const { data, isLoading, error } = useFetchData(
+//     "http://localhost:8080/api/service-requests/user-requests"
+//   );
+
+//   // Render loading or error state if necessary
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (error) {
+//     return <div>Error: {error.message}</div>;
+//   }
+
+//   // Render your component with the fetched data
+//   return <div>{/* Render your component with the fetched data */}</div>;
+// };
