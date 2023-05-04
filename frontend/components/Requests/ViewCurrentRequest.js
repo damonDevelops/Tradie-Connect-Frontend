09cd2f44f6193f5d86a51c2c3bc1d2fd50440b56
essej93 <86395968+essej93@users.noreply.github.com>
2023-05-04 16:29:41 +1000

    Updated view service request: - added grid to display data - added functions to edit data display

--- frontend/components/Requests/ViewCurrentRequest.js
+++ frontend/components/Requests/ViewCurrentRequest.js
@@ -4,32 +4,15 @@ import Paper from "@mui/material/Paper";
 
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import axios from "axios";
 import { useEffect } from "react";
 
 import { useRouter } from "next/router";
 
-import { Link, TextField } from "@mui/material";
-import { useState } from "react";
+import { Grid, Link, TextField } from "@mui/material";
 
-// for displaying data:
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import TablePagination from "@mui/material/TablePagination";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 import useFetchData from "../hooks/fetchData";
-
-//import { DataGrid } from "@mui/x-data-grid";
 
 const theme = createTheme();
 
@@ -40,32 +23,13 @@ export default function ViewRequest() {
   const fetchURL =
     "http://localhost:8080/api/service-requests/" + router.query.id;
 
-  
   const { data: responseData } = useFetchData(fetchURL);
-  
 
   useEffect(() => {
     if (!fromRequests) {
       router.replace("/Customer/CurrentRequest");
     }
   }, [fromRequests, router]);
-
-  console.log(requestId);
-  console.log(fromRequests);
-  console.log("original response data:")
-  console.log(responseData);
-  //console.log(displayData);
-
-  //const displayData = editDisplayData(responseData);
-  console.log("After data format: ")
-  //console.log(displayData)
-
-  const disabledStyle = {
-    "&.MuiInputBase-disabled": {
-      bgcolor: "#CEEAD0",
-      color: "green",
-    },
-  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -79,125 +43,95 @@ export default function ViewRequest() {
         }}
       >
         <Typography variant="h4" gutterBottom>
-          View Request: {requestId}
-           <Box sx={{ p: 2 }}>
+          Service Request Details
+          <Box sx={{ p: 2 }}>
             {responseData ? (
               <>
-                <Typography variant="h4" sx={{ mb: 2 }}>
-                  Service Request Details
-                </Typography>
-                <Box sx={{ display: "flex", flexDirection: "column" }}>
-                  <TextField
-                    label="Request ID"
-                    value={responseData.id}
-                    disabled
-                    InputLabelProps={{ shrink: true }}
-                    sx={{ mb: 2 }}
-                  />
-                  <TextField
-                    label="Date Submitted"
-                    value={responseData.requestedDate}
-                    disabled
-                    InputLabelProps={{ shrink: true }}
-                    sx={{ mb: 2 }}
-                  />
-                  <TextField
-                    label="Scheduled Date"
-                    value={responseData.scheduledStartDate}
-                    disabled
-                    InputLabelProps={{ shrink: true }}
-                    sx={{ mb: 2 }}
-                  />
-                  <TextField
-                    label="Scheduled Finish"
-                    value={responseData.scheduledStartDate}
-                    disabled
-                    InputLabelProps={{ shrink: true }}
-                    sx={{ mb: 2 }}
-                  />
-                  <TextField
-                    label="Status: "
-                    value={responseData.status}
-                    disabled
-                    InputLabelProps={{ shrink: true }}
-                    sx={{ mb: 2 }}
-                  />
-                  <TextField
-                    label="Description"
-                    value={responseData.description}
-                    disabled
-                    InputLabelProps={{ shrink: true }}
-                    sx={{ mb: 2 }}
-                  />
-                  {/* <Box
-                    sx={{ display: "flex", justifyContent: "space-between" }}
-                  >
-                    <Button onClick={handleBackButtonClick}>Back</Button>
-                  </Box> */}
-                </Box>
+                <Grid container spacing={1}>
+                  <Grid item xs={12} md={4}>
+                    <TextField
+                      label="Request ID"
+                      value={responseData.id}
+                      disabled
+                      InputLabelProps={{ shrink: true }}
+                      sx={{ mb: 2 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={4}>
+                    <TextField
+                      label="Status: "
+                      value={capitaliseWords(responseData.status)}
+                      disabled
+                      InputLabelProps={{ shrink: true }}
+                      sx={{ mb: 2 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={4}>
+                    <TextField
+                      label="Type: "
+                      value={capitaliseWords(responseData.serviceType)}
+                      disabled
+                      InputLabelProps={{ shrink: true }}
+                      sx={{ mb: 2 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={4}>
+                    <TextField
+                      label="Date Submitted"
+                      value={formatDate(responseData.requestedDate)}
+                      disabled
+                      InputLabelProps={{ shrink: true }}
+                      sx={{ mb: 2 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={4}>
+                    <TextField
+                      label="Scheduled Date"
+                      value={formatDate(responseData.scheduledStartDate)}
+                      disabled
+                      InputLabelProps={{ shrink: true }}
+                      sx={{ mb: 2 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} md={4}>
+                    <TextField
+                      label="Scheduled Finish"
+                      value={formatDate(responseData.scheduledStartDate)}
+                      disabled
+                      InputLabelProps={{ shrink: true }}
+                      sx={{ mb: 2 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      label="Description"
+                      value={responseData.description}
+                      disabled
+                      InputLabelProps={{ shrink: true }}
+                      multiline={true}
+                      fullWidth
+                    />
+                  </Grid>
+                </Grid>
               </>
             ) : (
               <Typography variant="h6">Loading...</Typography>
             )}
-          </Box> 
-          {/* {fromRequests ? (
-            <Typography>
-              you came here from the current requests page
-            </Typography>
-          ) : (
-            <Typography>
-              you naughty naughty, trying to access this page directly
-            </Typography>
-          )} */}
+          </Box>
         </Typography>
       </Paper>
     </ThemeProvider>
   );
 }
 
-// export function editDisplayData(data) {
-//   const [displayData, setDisplayData] = useState({
-//     serviceType: "",
-//     requestedDate: "",
-//     requestedTime: "",
-//     scheduledStartDate: "",
-//     scheduledStartTime: "",
-//     scheduledEndDate: "",
-//     scheduledEndTime: "",
-//     cost: "",
-//     description: "",
-//   });
+function capitaliseWords(str) {
+  return str
+    .toLowerCase()
+    .split("_")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
 
-//   useEffect(() => {
-//     const capitaliseWords = (str) => {
-//       return str
-//         .toLowerCase()
-//         .split("_")
-//         .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-//         .join(" ");
-//     };
-
-//     const formatDate = (date) => {
-//       return date[2] + "/" + date[1] + "/" + date[0];
-//     };
-
-//     if (data) {
-//       setDisplayData({
-//         serviceType: capitaliseWords(data.serviceType),
-//         requestedDate: formatDate(data.requestedDate),
-//         requestedTime: data.requestedTime,
-//         scheduledStartDate: formatDate(data.scheduledStartDate),
-//         scheduledStartTime: data.scheduledStartTime,
-//         scheduledEndDate: formatDate(data.scheduledEndDate),
-//         scheduledEndTime: data.scheduledEndTime,
-//         cost: data.cost,
-//         description: data.description,
-//         status: capitaliseWords(data.status),
-//       });
-//     }
-
-
-//   }, [data]);
-
-//   return displayData;
-// }
+function formatDate(date) {
+  return date[2] + "/" + date[1] + "/" + date[0];
+}
