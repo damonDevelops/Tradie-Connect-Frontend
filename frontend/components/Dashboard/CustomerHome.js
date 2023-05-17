6d90e35194c650397bcdb7de3c6b095656d7c83e
essej93 <86395968+essej93@users.noreply.github.com>
2023-05-17 15:36:48 +1000

    updated import for CurrentRequest

--- frontend/components/Dashboard/CustomerHome.js
+++ frontend/components/Dashboard/CustomerHome.js
@@ -7,7 +7,7 @@ import CustomersTable from "../Admin/CustomersTable";
 import ServiceProvidersTable from "../Admin/ServiceProvidersTable";
 import RequestsTable from "../Admin/RequestsTable";
 import { Button, Divider } from "@mui/material";
-import CurrentRequest from "../Requests/CurrentRequest";
+import CurrentRequest from "../Requests/CustomerCurrentRequest";
 import Grid from "@mui/material/Grid";
 import Item from "@mui/material/Grid";
 import Link from "next/link";
@@ -35,71 +35,78 @@ export default function Home() {
               Make a New Request{" "}
             </Typography>
             <br />
-            <Link href="/Customer/NewRequest" passHref legacyBehavior color="inherit">
-            <Button
-              variant="contained"
-              sx={{ width: "100%" }}
-            >
-              New Request{" "}
-            </Button>
-            </Link>
-            
-          </Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper
-            sx={{
-              p: 2,
-              width: "100%",
-              display: "flex",
-              flexDirection: "column",
-              height: "auto",
-              marginTop: 2,
-            }}
-          >
-            <Typography variant="h5" gutterBottom>
-              Past Requests{" "}
-            </Typography>
-            <br />
-            <Link href="/Customer/PastRequests" passHref legacyBehavior color="inherit">
-            <Button
-              variant="contained"
-              sx={{ width: "100%" }}
-            >
-              Past Requests{" "}
-            </Button>
-            </Link>
-            
-          </Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper
-            sx={{
-              p: 2,
-              width: "100%",
-              display: "flex",
-              flexDirection: "column",
-              height: "auto",
-              marginTop: 2,
-            }}
-          >
-            <Typography variant="h5" gutterBottom>
-              Reports{" "}
-            </Typography>
-            <br />
-            <Link href="/Customer/Reports" passHref legacyBehavior color="inherit">
-            <Button
-              variant="contained"
-              sx={{ width: "100%" }}
+            <Link
               href="/Customer/NewRequest"
+              passHref
+              legacyBehavior
+              color="inherit"
             >
-              Reports{" "}
-            </Button>
+              <Button variant="contained" sx={{ width: "100%" }}>
+                New Request{" "}
+              </Button>
             </Link>
           </Paper>
         </Grid>
-        <Grid item xs={12} sx={{mt: 3}}>
-        <CurrentRequest />
+        <Grid item xs={4}>
+          <Paper
+            sx={{
+              p: 2,
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              height: "auto",
+              marginTop: 2,
+            }}
+          >
+            <Typography variant="h5" gutterBottom>
+              Past Requests{" "}
+            </Typography>
+            <br />
+            <Link
+              href="/Customer/PastRequests"
+              passHref
+              legacyBehavior
+              color="inherit"
+            >
+              <Button variant="contained" sx={{ width: "100%" }}>
+                Past Requests{" "}
+              </Button>
+            </Link>
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper
+            sx={{
+              p: 2,
+              width: "100%",
+              display: "flex",
+              flexDirection: "column",
+              height: "auto",
+              marginTop: 2,
+            }}
+          >
+            <Typography variant="h5" gutterBottom>
+              Reports{" "}
+            </Typography>
+            <br />
+            <Link
+              href="/Customer/Reports"
+              passHref
+              legacyBehavior
+              color="inherit"
+            >
+              <Button
+                variant="contained"
+                sx={{ width: "100%" }}
+                href="/Customer/NewRequest"
+              >
+                Reports{" "}
+              </Button>
+            </Link>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sx={{ mt: 3 }}>
+          <CurrentRequest />
         </Grid>
       </Grid>
     </ThemeProvider>
